c4bfce042e32d140e82ce44af99ef3f6c62dfbeb
stanislav.knyazev <knyazev@ziex.by>
2012-09-25 17:58:25 +0300

    Added seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+MusicAlbum.create :id =>6, :name => "…And Justice for All", :year => 1988
+Track.create :id => 30, :name => "...And Justice for All", :duration => 582, :music_album_id => 6
+Track.create :id => 31, :name => "One", :duration => 390, :music_album_id => 6
+Track.create :id => 32, :name => "Some song", :duration => 312, :music_album_id => 6
+Track.create :id => 33, :name => "Hit the Lights", :duration => 257, :music_album_id => 6
+Track.create :id => 34, :name => "Whiplash", :duration => 250, :music_album_id => 6
+MusicAlbum.create :id =>7, :name => "Kill Em All", :year => 1983
+Track.create :id => 42, :name => "Eye of the Beholder", :duration => 390, :music_album_id => 7
+Track.create :id => 43, :name => "Jump in the Fire", :duration => 312, :music_album_id => 7
+Track.create :id => 44, :name => "Hit the Lights", :duration => 257, :music_album_id => 7
+Track.create :id => 45, :name => "Whiplash", :duration => 250, :music_album_id => 7
