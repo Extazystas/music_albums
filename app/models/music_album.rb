5b7eacd2026b8609b9ec39d10ec259a572da5af4
stanislav.knyazev <knyazev@ziex.by>
2012-09-25 12:22:27 +0300

    Added music_album_id to tracks table

--- app/models/music_album.rb
+++ app/models/music_album.rb
@@ -1,7 +1,7 @@
 class MusicAlbum < ActiveRecord::Base
   attr_accessible :name, :year
 
-  has_many :tracks
+  has_many :tracks, :dependent => :destroy
 
   validates :name, presence: true
   validates :year, numericality: {only_integer: true, greater_than: 0}, length: {is: 4}, allow_blank: true
